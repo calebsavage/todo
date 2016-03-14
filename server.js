657b0321c847c316ae9c7d9b93457cc2fb89666e
Caleb Savage <calebsavagephoto@gmail.com>
2016-03-14 16:33:43 -0400

    changed to port 80

--- server.js
+++ server.js
@@ -68,5 +68,5 @@ app.delete('/todo/:id', function(req,res){
   });
 });
 
-app.listen(3000);
-console.log("server running on port 3000  ");
+app.listen(80);
+console.log("server running on port 80  ");
